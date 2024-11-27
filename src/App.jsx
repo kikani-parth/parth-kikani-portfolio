b7af38c4eb504a14cfb4fe9ed066854d4cd59ead
Parth Kikani <parthkikani02@gmail.com>
2024-11-27 11:48:26 +0200

    add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@
  * @license Apache-2.0
  */
 
+/**
+ * Components
+ */
+import Header from "./components/Header";
+
 const App = () => {
-  return <h1>Hello</h1>
+  return <Header />
 }
 
 export default App;

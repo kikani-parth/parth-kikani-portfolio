b7761d5ceee453f316f2430bf60be2fa5afb3d6e
Parth Kikani <parthkikani02@gmail.com>
2024-11-27 10:53:40 +0200

    add custom scrollbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,24 @@
+/**
+ * @copyright 2024 Parth Kikani
+ * @license Apache-2.0
+ */
+
+/**
+ * Node modules
+ */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+/**
+ * Components
+ */
 import App from './App.jsx'
+
+
+/** CSS link
+ * 
+ */
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

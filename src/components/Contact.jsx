dd51b4d44e2be5756f77fd0dea24f90be2c1d9bf
Parth Kikani <parthkikani02@gmail.com>
2024-12-03 09:02:36 +0200

    add sroll reveal animation to Contact and Work sections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -91,11 +91,13 @@ const socialLinks = [
 const Contact = () => {
   return (
     <section id="contact" className="section">
-      <div className="container lg:grid lg:grid-cols-2 lg:items-stretch">
+      <div className="container lg:grid lg:grid-cols-2 lg:items-stretch mb-10">
         <div className="mb-12 lg:mb-0 lg:flex lg:flex-col">
-          <h2 className="headline-2 lg:max-w-[12ch]">Let&apos;s talk</h2>
+          <h2 className="headline-2 lg:max-w-[12ch] reveal-up">
+            Let&apos;s talk
+          </h2>
 
-          <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch] lg:max-w-[30ch]">
+          <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch] lg:max-w-[30ch] reveal-up">
             I&apos;m currently available to take on new projects/jobs, so feel
             free to send me a message to start our exciting journey together!
           </p>
@@ -106,7 +108,7 @@ const Contact = () => {
                 key={key}
                 href={href}
                 target="_blank"
-                className="w-12 h-12 grid place-items-center ring-inset ring-2 ring-zinc-50/5 rounded-lg transition-[background-color,color] hover:bg-zinc-50 hover:text-zinc-950 active:bg-zinc-50/80"
+                className="w-12 h-12 grid place-items-center ring-inset ring-2 ring-zinc-50/5 rounded-lg transition-[background-color,color] hover:bg-zinc-50 hover:text-zinc-950 active:bg-zinc-50/80 reveal-up"
               >
                 {icon}
               </a>
@@ -136,7 +138,7 @@ const Contact = () => {
 
           <div className="md:grid md:items-center md:grid-cols-2 md:gap-2">
             <div className="mb-4">
-              <label htmlFor="name" className="label">
+              <label htmlFor="name" className="label reveal-up">
                 Name
               </label>
 
@@ -147,12 +149,12 @@ const Contact = () => {
                 autoComplete="name"
                 required
                 placeholder="Parth Kikani"
-                className="text-field"
+                className="text-field reveal-up"
               />
             </div>
 
             <div className="mb-4">
-              <label htmlFor="email" className="label">
+              <label htmlFor="email" className="label reveal-up">
                 Email
               </label>
 
@@ -163,13 +165,13 @@ const Contact = () => {
                 autoComplete="email"
                 required
                 placeholder="parth@example.com"
-                className="text-field"
+                className="text-field reveal-up"
               />
             </div>
           </div>
 
           <div className="mb-4">
-            <label htmlFor="message" className="label">
+            <label htmlFor="message" className="label reveal-up">
               Message
             </label>
 
@@ -178,13 +180,13 @@ const Contact = () => {
               id="message"
               placeholder="Hi!"
               required
-              className="text-field resize-y min-h-32 max-h-80"
+              className="text-field resize-y min-h-32 max-h-80 reveal-up"
             ></textarea>
           </div>
 
           <button
             type="submit"
-            className="btn btn-primary [&]:max-w-full w-full justify-center"
+            className="btn btn-primary [&]:max-w-full w-full justify-center reveal-up"
           >
             Submit
           </button>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -49,7 +49,7 @@ const Work = () => {
   return (
     <section id="work" className="section">
       <div className="container">
-        <h2 className="headline-2 mb-8"> My Portfolio Highlights</h2>
+        <h2 className="headline-2 mb-8 reveal-up"> My Portfolio Highlights</h2>
         <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
           {works.map(({ imgSrc, title, tags, projectLink }, key) => (
             <ProjectCard
@@ -58,6 +58,7 @@ const Work = () => {
               title={title}
               tags={tags}
               projectLink={projectLink}
+              classes="reveal-up"
             />
           ))}
         </div>

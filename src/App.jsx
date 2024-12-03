9562bb2c1583f89c0453a6c1d8a8f4a16a109298
Parth Kikani <parthkikani02@gmail.com>
2024-12-03 08:32:40 +0200

    add gsap scroll reveal animation to About and Skill section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,14 @@
  * Node modules
  */
 import { ReactLenis } from 'lenis/react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+/**
+ * Register gsap plugins
+ */
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 /**
  * Components
@@ -19,6 +27,25 @@ import Work from './components/Work';
 import Contact from './components/Contact';
 
 const App = () => {
+  useGSAP(() => {
+    const elements = gsap.utils.toArray('.reveal-up');
+
+    elements.forEach((element) => {
+      gsap.to(element, {
+        scrollTrigger: {
+          trigger: element,
+          start: '-200 bottom',
+          end: 'bottom 80%',
+          scrub: true,
+        },
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: 'power2.out',
+      });
+    });
+  });
+
   return (
     <ReactLenis root>
       <Header />
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
   return (
     <section id="about" className="section">
       <div className="container">
-        <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12">
+        <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up">
           <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
             Hi, I&apos;m Parth, a fourth-year Information Technology student
             proficient in IoT, Web and Mobile development. I combine theoretical
